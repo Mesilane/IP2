5e4fd6197856352270c878bb29e499ea86507acb
Mesilane <mesilaned@gmail.com>
2024-01-04 16:21:21 +0300

    Сделал кнопки рабочими

--- funk.js
+++ funk.js
@@ -1,9 +1,21 @@
+var di
 
 let apps = document.querySelectorAll('.apps')
 for (let i of apps){
     i.addEventListener('click', function(e) {
         document.querySelector('#mainMenu').style.display = 'none'
-        document.querySelectorAll(`#${this.id}`)[1].style.display = 'inline'
+        document.querySelector('.arrow').style.display = 'inline'
+        document.querySelector('.drag p').style.margin = '0 0 0 32px'
+        document.querySelector(`.${this.id}`).style.display = 'inline'
+        di = this.id
     })
 }
+
+document.querySelector('.arrow').addEventListener('click', function(e) {
+    document.querySelector('.arrow').style.display = 'none'
+    document.querySelector(`.${di}`).style.display = 'none'
+    document.querySelector('#mainMenu').style.display = 'inline'
+    document.querySelector('.drag p').style.margin = '0 0 0 -3px'
+})
+
 
