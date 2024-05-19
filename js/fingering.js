526c20684aecbb85c86c8cdaa58216f24d9c89ac
Mesilane <mesilaned@gmail.com>
2024-05-19 17:33:07 +0300

    made fingering

--- js/fingering.js
+++ js/fingering.js
@@ -10,9 +10,27 @@ for (let i of document.querySelectorAll('.selector')){
         arr[String(this.id)] = this.value
         window.api.sendNote(Object.values(arr))
         window.api.callNote((arg) =>{
-            for (let i of arg){
-                
+            for(i of document.querySelectorAll('.active')){
+                i.style.backgroundImage = 'none'
+                i.classList.remove('active')
             }
+            if (arg[0].length > 4){
+                // for(i of document.querySelectorAll('col')){
+                //     for(let b of i.childNodes){
+                //         b.style.backgroundImage = 'none'
+                //     }
+                // }
+                // for(i of document.querySelectorAll('active')){
+                //     i.style.backgroundImage = 'none'
+                // }
+                console.log(arg)
+                for(let i = 0; i < 6; i++){
+                    a = document.querySelectorAll('.col')[i].querySelector(`.p${arg[0][i]}`)
+                    console.log(a)
+                    a.style.backgroundImage = `url(../icons/pos/${arg[0][i]}.png)`
+                    a.classList.add('active')
+            }
+        }
         })
     })
 }
